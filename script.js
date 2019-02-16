e9651c1d648b72645e77af4377ef0d2e046bfd1e
Max Pikulin <trurl1000@gmail.com>
2019-02-16 15:31:39 +0000

    fetch and display all tasks.

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 containerDiv = document.querySelector('.container');
+// const body = {
+//   query: 'select * from SSD2TB order by id desc',
+// };
 const body = {
-  query: 'select * from SSD2TB order by id desc',
+  query: 'select tableName from tasks',
 };
-const ws = new WebSocket('ws://zavit.com:8010');
+// const ws = new WebSocket('ws://zavit.com:8010');
 
-ws.onopen = function() {
-  ws.send('Hello Server!');
-}
-ws.onmessage = function(event) {
-  prependRow(event.data);
-}
+// ws.onopen = function() {
+//   ws.send('Hello Server!');
+// }
+// ws.onmessage = function(event) {
+//   prependRow(event.data);
+// }
 
 function prependRow(data) {
   containerDiv.innerHTML = `<div class="newData">${data}</div>` + containerDiv.innerHTML;
@@ -21,7 +24,20 @@ async function init() {
     body: JSON.stringify(body),
   });
   result = await result.json();
-  containerDiv.innerHTML = makeTable(result);
+  // console.log(result);
+  for (let i = 0; i < result.length; i++) {
+    // console.log(result[i].tableName);
+    let table = await fetch('../api/mysqlAPI.js', {
+      method: 'POST',
+      body: JSON.stringify({
+        query: `select * from ${result[i].tableName} order by id desc`,
+      }),
+    });
+    table = await table.json();
+    // console.log(table);
+    containerDiv.innerHTML += `<div>${result[i].tableName}</div>` + makeTable(table);
+  }
+  // containerDiv.innerHTML = makeTable(result);
 }
 
 function makeTable(data) {
